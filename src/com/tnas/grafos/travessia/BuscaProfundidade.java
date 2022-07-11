31474de3cbaeae933e03f6d2a4d4cba17070497a
Thiago Nascimento <nascimenthiago@gmail.com>
2022-07-11 08:47:46 -0300

    Rastreando caminho em busca de profundidade

--- src/com/tnas/grafos/travessia/BuscaProfundidade.java
+++ src/com/tnas/grafos/travessia/BuscaProfundidade.java
@@ -1,5 +1,6 @@
 package com.tnas.grafos.travessia;
 
+import java.util.Objects;
 import java.util.Stack;
 
 import com.tnas.grafos.representacoes.ListaAdjacencia;
@@ -27,6 +28,45 @@ public class BuscaProfundidade {
 				listaAdjacencia.obterAdjacentes(vertice).stream()
 					.filter(v -> v.getStatus() == StatusVisitaVertice.NOVO)
 					.forEach(v -> pilha.push(v));
+			}
+		}
+	}
+	
+	public void executarBusca(ListaAdjacencia listaAdjacencia, Vertice origem, Vertice destino) {
+		
+		var pilha = new Stack<Vertice>();
+		
+		listaAdjacencia.obterVertices().forEach(v -> { 
+			v.setStatus(StatusVisitaVertice.NOVO);
+			v.setPai(null);
+		});
+		
+		pilha.push(listaAdjacencia.obterVertice(origem.getNome()));
+		
+		while (!pilha.empty()) {
+			
+			var vertice = pilha.pop();
+			
+			if (vertice.getNome() == destino.getNome()) {
+				
+				while (Objects.nonNull(vertice)) {
+					System.out.print(vertice.getNome() + " ");
+					vertice = vertice.getPai();
+				}
+				
+				break;
+			}
+			
+			if (vertice.getStatus() == StatusVisitaVertice.NOVO) {
+				
+				vertice.setStatus(StatusVisitaVertice.VISITADO);
+				
+				for (var v : listaAdjacencia.obterAdjacentes(vertice)) {
+					if (v.getStatus() == StatusVisitaVertice.NOVO) {
+						pilha.push(v);
+						v.setPai(vertice);
+					}
+				}
 			}
 		}
 	}
--- test/com/tnas/grafos/travessia/BuscaProfundidadeTest.java
+++ test/com/tnas/grafos/travessia/BuscaProfundidadeTest.java
@@ -6,8 +6,16 @@ import com.tnas.grafos.representacoes.Vertice;
 public class BuscaProfundidadeTest {
 
 	public static void main(String[] args) {
+		
 		var testeBuscaProfundidade = new BuscaProfundidadeTest();
+		
+		System.out.println("*** Teste 1 ***");
 		testeBuscaProfundidade.executarTeste1();
+		System.out.println();
+		
+		System.out.println("*** Teste 2 ***");
+		testeBuscaProfundidade.executarTeste2();
+		System.out.println();
 	}
 	
 	public void executarTeste1() {
@@ -21,6 +29,22 @@ public class BuscaProfundidadeTest {
 		
 		var buscaProfundidade = new BuscaProfundidade();
 		buscaProfundidade.executarBusca(new ListaAdjacencia(numVertices, arestas), new Vertice(0));
+	}
+	
+	public void executarTeste2() {
+		
+		var numVertices = 13;
+		Integer[][] arestas = { 
+				{ 0, 1 }, { 0, 5 }, { 2, 0 }, { 2, 3 }, { 3, 2 }, 
+				{ 3, 5 }, { 4, 2 }, { 4, 3 }, { 5, 4 }, { 6, 4 }, 
+				{ 6, 9 }, { 6, 0 }, { 7, 6 }, { 7, 8 }, { 8, 7 }, 
+				{ 8, 9 }, { 9, 10 }, { 9, 11 }, { 10, 12 }, 
+				{ 11, 4 }, { 11, 12 }, { 12, 9 }
+		};
+		
+		var buscaProfundidade = new BuscaProfundidade();
+		buscaProfundidade.executarBusca(new ListaAdjacencia(numVertices, arestas), 
+				new Vertice(10), new Vertice(0));
 	}
 
 }
